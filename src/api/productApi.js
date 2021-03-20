90ba44a36295a2f3d471cd5119fba3f95b2d398a
Cory Siebler <cory.siebler@protonmail.com>
2021-03-20 03:23:57 -0700

    Fixing other BASE_URL for product API on production

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://enigmatic-waters-63171.herokuapp.com/"
+    ? "https://enigmatic-waters-63171.herokuapp.com"
     : "http://localhost:3001";
 
 export const getProducts = () => {
