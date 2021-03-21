e6c5d77aadee9607fbec4b965fa3b65196fd5113
Cory Siebler <cory.siebler@protonmail.com>
2021-03-21 07:13:48 -0700

    Updated Skeleton theme to match beige background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/main.scss";
 import { StylesProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { SkeletonTheme } from "react-loading-skeleton";
 import { DisplayProvider } from "./context/displayContext";
 import { ProductProvider } from "./context/productContext";
 import LandingPage from "./pages/LandingPage";
@@ -8,12 +9,14 @@ import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <StylesProvider injectFirst>
-      <CssBaseline />
-      <DisplayProvider>
-        <ProductProvider>
-          <LandingPage />
-        </ProductProvider>
-      </DisplayProvider>
+      <SkeletonTheme color="#dcdcf5" highlightColor="#c6c6dc">
+        <CssBaseline />
+        <DisplayProvider>
+          <ProductProvider>
+            <LandingPage />
+          </ProductProvider>
+        </DisplayProvider>
+      </SkeletonTheme>
     </StylesProvider>
   );
 }
